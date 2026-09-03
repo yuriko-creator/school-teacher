48fecf45936dc01fca49608b54323422dd352c49
Yuriko <yurikandryshin@gmail.com>
2026-09-03 15:45:21 +0300

    1.1

--- safety_at_school_and_with_a_computer_files/_clientMiddlewareManifest.js
+++
@@ -1,6 +0,0 @@
-self.__MIDDLEWARE_MATCHERS = [
-  {
-    "regexp": "^(?:\\/(_next\\/data\\/[^/]{1,}))?\\/published(?:\\/((?:[^\\/#\\?]+?)(?:\\/(?:[^\\/#\\?]+?))*))?(\\.json|\\.rsc|\\.segments\\/.+\\.segment\\.rsc)?[\\/#\\?]?$",
-    "originalSource": "/published/:docId*"
-  }
-];self.__MIDDLEWARE_MATCHERS_CB && self.__MIDDLEWARE_MATCHERS_CB()
